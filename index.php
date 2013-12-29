b405dc333b27d187ac2286c45c6262fb7d15587c
Такитычъ <takiti4@kavanga.ru>
2013-12-29 20:34:37 +0400

    11

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Eva</title>
+        <link rel="stylesheet" type="text/css" href="m/c/g.css">
+    </head>
+    <body>
+
+    </body>
+</html>
