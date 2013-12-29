a909158ed67adc8af431092c50e018e816b77c1a
Такитычъ <takiti4@kavanga.ru>
2013-12-29 23:03:34 +0400

    11

--- index.php
+++ index.php
@@ -4,8 +4,113 @@
         <meta charset="utf-8">
         <title>Eva</title>
         <link rel="stylesheet" type="text/css" href="m/c/g.css">
+        <link rel="stylesheet" type="text/css" href="m/c/mcs.css">
+        <script src="m/j/jquery-1.9.1.min.js"></script>
+        <script src="m/j/jquery.mousewheel.min.js"></script>
+        <script src="m/j/jquery.mCustomScrollbar.min.js"></script>
+        <script>
+            (function($){
+                $(window).load(function(){
+                    $(".left").mCustomScrollbar();
+                });
+            })(jQuery);
+        </script>
     </head>
     <body>
+        <div class="body">
+            <div class="head">
+                <a class="logo" href="/" title="EVE Online - CNTL"></a>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a>Общий мир</a>
+                        </li>
+                        <li>
+                            <a>Своя Судьба</a>
+                        </li>
+                        <li>
+                            <a>Мультимедия</a>
+                        </li>
+                        <li>
+                            <a>Друзья</a>
+                        </li>
+                        <li>
+                            <a>Retrebution</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="con">
+                <div class="left">
+                    fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>fdvd vdfv vfd<br>
+                    v df<br><br><br><br><br>
+                    v df<br><br><br><br>
+                    v d<br><br><br>
+                </div>
+                <div class="right">
 
+                </div>
+            </div>
+        </div>
     </body>
 </html>
