c0996c0b052a0eb959b6677d4463f1f0dadbb250
Такитычъ <takiti4@kavanga.ru>
2014-01-08 20:59:57 +0400

    11

--- index.php
+++ index.php
@@ -42,70 +42,71 @@
             </div>
             <div class="con">
                 <div class="left">
-                    fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>fdvd vdfv vfd<br>
-                    v df<br><br><br><br><br>
-                    v df<br><br><br><br>
-                    v d<br><br><br>
+
+                    <div class="help-box">
+                        <div class="item">
+                            <div class="_l">Форумы</div>
+                            <div class="_r">Начните общаться с другими пилотами.</div>
+                        </div>
+                        <div class="item">
+                            <div class="_l">Информация для игроков</div>
+                            <div class="_r">Всё, что нужно для принятия решений.</div>
+                        </div>
+                        <div class="item">
+                            <div class="_l">Eva Gate</div>
+                            <div class="_r">Оставайтесь на связи с друзьями.</div>
+                        </div>
+                        <div class="item">
+                            <div class="_l">Ваша учетная запись</div>
+                            <div class="_r">Оплата подписки и прочие операции.</div>
+                        </div>
+                        <div class="item">
+                            <div class="_l">Служба поддержки</div>
+                            <div class="_r">Получите помощь от наших специалистов.</div>
+                        </div>
+                        <div class="item">
+                            <div class="_l">Обновление игры</div>
+                            <div class="_r">Ознакомтесь со списком нововведений.</div>
+                        </div>
+                    </div>
+
+
+                    <div class="con-bg">
+                        <div class="img full_width iHome">
+
+                        </div>
+                        <div class="text full_width">
+                            <p>
+                            Мир EVE Online - это своего рода конструктор;
+                            его детали созданы разработчиками игры,
+                            однако играют ими десятки тысяч человек, взаимодействующих друг с другом в рамках единого сервера.
+                            Что именно соберут из этих деталей пилоты - решать им.
+                            По их воле в Новом Эдеме гибнут корабли, появляются процветающие корпорации, уходят в небытие могущественные империи.
+                            Каждое действие, совершаемое игроком, влияет на окружающий его мир - а он, в свою очередь, влияет на всех остальных игроков.
+                            </p><p>
+                            EVE Online - это игра, cюжет которой целиком и полностью определяется играющими в неё людьми.
+                            Сложнейшая система связей между ними делает жизнь Нового Эдема реалистично непредсказуемой;
+                            порой достаточно одной сделки, одного выстрела, одного слова, чтобы жизнь десятков тысяч пилотов изменилась навсегда.
+                            </p>
+                        </div>
+                        <div class="text col3">
+                            <span>
+                                <h3>ВЫБЕРИ КОРАБЛЬ</h3>
+                                <p>К твоим услугам сотни разнообразных моделей кораблей — для решения любых задач, на любой вкус.</p>
+                            </span>
+                            <span>
+                                <h3>СДЕЛАЙ ЕГО УНИКАЛЬНЫМ</h3>
+                                <p>При помощи сменных модулей любой корабль можно изменить до неузнаваемости.</p>
+                            </span>
+                            <span>
+                                <h3>СОБЕРИ КОЛЛЕКЦИЮ</h3>
+                                <p>На каждой станции у тебя есть безразмерный ангар, способный вместить все купленные корабли.</p>
+                            </span>
+                        </div>
+
+                    </div>
+
+
                 </div>
                 <div class="right">
 
